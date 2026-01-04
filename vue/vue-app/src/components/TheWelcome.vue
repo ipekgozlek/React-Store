cb8f9d72fd6edcbddb7a3fb4f7eed3cb215736dc
İpek Gözlek <ipekgozlek@gmail.com>
2026-01-04 22:32:04 +0300

    vue-svelte-app

--- vue/vue-app/src/components/TheWelcome.vue
+++ vue/vue-app/src/components/TheWelcome.vue
@@ -1,7 +1,18 @@
 <script setup>
-  import { useCounterStore } from '@/stores/counter';
+import { useCounterStore } from '@/stores/counter'
+import { storeToRefs } from 'pinia';
+
+const counter = useCounterStore()
 </script>
 
 <template>
+  <div class="welcome">
+    <h1>Welcome to Your Vue.js App</h1>
 
+    <div class="counter">
+      <p>Current Count: {{ counter.count }}</p>
+      <p>Double Count: {{ counter.doubleCount }}</p>
+      <button @click="counter.increment">Increment Count</button>
+    </div>
+  </div>
 </template>
